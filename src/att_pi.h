7da3933a78baa70a4adcb60fe5491327b03c8e0b
Rick Gleason <fcgleason@tgp-architects.com>
2023-09-15 12:32:44 -0400

    0.1.7.0 streamline + libs

--- src/att_pi.h
+++ src/att_pi.h
@@ -32,11 +32,6 @@
   #include "wx/wx.h"
 #endif //precompiled headers
 
-//#define     PLUGIN_VERSION_MAJOR    0
-//#define     PLUGIN_VERSION_MINOR    0
-
-//#define     MY_API_VERSION_MAJOR    1
-//#define     MY_API_VERSION_MINOR    9
 
 #include "version.h"
 
