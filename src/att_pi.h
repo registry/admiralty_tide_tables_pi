3ab16ab4cff6fb0923dc1a596f874888bc63d11f
Rick Gleason <fcgleason@tgp-architects.com>
2023-10-02 16:12:21 -0400

    0.1.10.0 + api 117

--- src/att_pi.h
+++ src/att_pi.h
@@ -45,8 +45,7 @@
 //    The PlugIn Class Definition
 //----------------------------------------------------------------------------------------------------------
 
-//class weather_routing_pi : public wxEvtHandler, public opencpn_plugin_116
-class att_pi : public opencpn_plugin_116
+class att_pi : public opencpn_plugin_117
 {
 public:
       att_pi(void *ppimgr);
@@ -60,7 +59,9 @@ public:
       int GetAPIVersionMinor();
       int GetPlugInVersionMajor();
       int GetPlugInVersionMinor();
-
+      int GetPlugInVersionPatch();
+      int GetPlugInVersionPost();
+	  
       wxString GetCommonName();
       wxString GetShortDescription();
       wxString GetLongDescription();
